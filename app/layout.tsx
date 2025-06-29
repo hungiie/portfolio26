950a9a5f23856bfe4202615256ac217396cf9b85
Duy hung Tran <duy_hung_tran@sfu.ca>
2025-06-29 15:36:09 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Nav/>
+        <Analytics />
         {children}
       </body>
     </html>
