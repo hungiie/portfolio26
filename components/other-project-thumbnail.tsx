9ddd851d1caf71f1321442c52e5e8d5328f2ca7b
Duy hung Tran <duy_hung_tran@sfu.ca>
2025-06-28 17:22:26 -0700

    Add other projects

---
+++ components/other-project-thumbnail.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+interface Props {
+  link: string;
+  name: string;
+  description: string;
+  tag: string;
+  imageSrc: string;
+}
+
+export default function OtherProjectThumbnail(props: Props) {
+    return(
+        <a href={props.link} className="group mb-8 block rounded-2xl border-[#e3e3e3] border-1 overflow-hidden hover:border-[var(--main-blue)]">
+            <img src={props.imageSrc} alt={props.name} className="w-full h-60 object-cover" />
+            <div className="px-5 pt-6 pb-8 w-full">
+                <div className="w-full flex items-center mb-3">
+                    <p className={`text-md text-[var(--colour-bodytext)] group-hover:underline group-hover:text-[var(--main-blue)]`}>
+                        {props.name}
+                    </p>
+                </div>
+                <p className="mb-3 text-sm text-[var(--colour-bodytext)]">{props.description}</p>
+                <p className="mt-2 inline-block text-xs text-[#4F4F4F]">{props.tag}</p>
+            </div>
+        </a>
+    )
+}
--- components/project-thumbnail.tsx
+++ components/project-thumbnail.tsx
@@ -12,12 +12,14 @@ interface Props {
 export default function ProjectThumbnail(props: Props) {
     return(
         <a href={props.link} className="group mb-8 block rounded-2xl border-[#e3e3e3] border-1 overflow-hidden hover:border-[var(--main-blue)]">
-            <img src={props.imageSrc} alt={props.name} className="w-full h-60 object-cover transition-transform duration-400 group-hover:scale-103" />
-            <div className="px-7 pt-6 pb-8">
-                <p className="mb-3 text-md text-[var(--colour-bodytext)] group-hover:underline group-hover:text-[var(--main-blue)] flex items-center">
-                    {props.name}
-                    <span className="w-2 h-2 rounded-full inline-block ml-3" style={{ backgroundColor: props.colourCode }}></span>
-                </p>
+            <img src={props.imageSrc} alt={props.name} className="w-full h-60 object-cover" />
+            <div className="px-7 pt-6 pb-8 w-full">
+                <div className="w-full flex items-center mb-3">
+                    {/* <span className="w-2 h-2 rounded-full inline-block mr-3" style={{ backgroundColor: props.colourCode }}></span> */}
+                    <p className={`text-md text-[var(--colour-bodytext)] group-hover:underline group-hover:text-[var(--main-blue)]`}>
+                        {props.name}
+                    </p>
+                </div>
                 <p className="mb-3 text-sm text-[var(--colour-bodytext)]">{props.description}</p>
                 <p className="mt-2 inline-block text-xs text-[#4F4F4F]">{props.tag}</p>
             </div>
