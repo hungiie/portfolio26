cf372f8cd1a3fde2ae778d8c21c4b34bb21f1ee3
Duy hung Tran <duy_hung_tran@sfu.ca>
2025-06-28 17:29:32 -0700

    Added specific configuration options

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    loader: "custom",
+    loaderFile: "./ImageLoader.js",
+  },
 };
 
 export default nextConfig;
