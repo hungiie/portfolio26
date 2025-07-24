2fe77d1242c7214ac2e9290251f5e8b810962cec
Duy hung Tran <duy_hung_tran@sfu.ca>
2025-07-23 19:05:00 -0700

    update footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,13 +6,13 @@ export default function Footer() {
             <p className="mb-1 text-xs text-[rgba(0,0,0,0.3)]">Designed and coded by me using Tailwind CSS and Next.js</p>
             <p className="mb-7 text-xs text-[rgba(0,0,0,0.3)]">© 2025 Hung. All rights reserved.</p>
             <div className="flex">
-                <a href="#" className="mr-3">
+                <a href="www.linkedin.com/in/hung-tran-profile" className="mr-3">
                     <Image src="/linkedin.png" alt="linkedin logo" width={20} height={20} />
                 </a>
-                <a href="#" className="mr-3">
-                    <Image src="/tiktok.png" alt="tiktok logo" width={20} height={20} />
+                <a href="https://github.com/hungiie" className="mr-3">
+                    <Image src="/github.png" alt="github logo" width={20} height={20} />
                 </a>
-                <a href="#" className="mr-3">
+                <a href="mailto:tranndhungg@gmail.com" className="mr-3">
                     <Image src="/mail.png" alt="mail logo" width={20} height={20} />
                 </a>
             </div>
